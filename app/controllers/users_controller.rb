ddd3585dcd036a96599f8019f9c6460213d22861
naotoshi-ikeda-creater <nao.0728.2017@gmail.com>
2020-01-19 22:04:45 +0900

    Add:add the comment function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def show
-        binding.pry
-        @nickname = current_user.nickname
-        @tweets =   @tweets = current_user.tweets.order("created_at DESC")
-    end 
-end
+      user = User.find(params[:id])
+      @nickname = user.nickname
+      @tweets = user.tweets.order("created_at DESC")
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new'=> 'tweets#new'
-  post'tweets' => 'tweets#create'
-  delete 'tweets/:id' => 'tweets#destroy'
-  get 'users/:id' => 'users#show'
+  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
 end
